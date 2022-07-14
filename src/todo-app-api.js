0f4199bf30417098be7eaa4f87d2f68174003802
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-07-14 17:39:41 -0300

    criacao da pasta src e arquivos

--- src/todo-app-api.js
+++ src/todo-app-api.js
@@ -6,7 +6,15 @@ const port = 3000
 app.get('/usuarios', (req, res) => {
     res.send('Rota ativada com GET e recurso usuarios: valores de usuarios devem ser retornados')
 })
+app.get('/tarefas', (req, res) => {
+    res.send('Rota ativada com GET e recurso tarefas: valores de tarefas devem ser retornados')
+})
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}/usuarios`)
+    console.log(`http://localhost:${port}/tarefas`)
 })
+
+export (app) => {
+    
+}
